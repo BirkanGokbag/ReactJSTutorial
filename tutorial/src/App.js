185c73f50813f07cffb19d78c954167860d21633
Michael Radey <radeymichael@gmail.com>
2018-11-06 11:25:00 -0500

    added form and navigation

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,194 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import Game from './Game';
+import SignIn from './SignIn';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      player1: 1,
-      player2: 2,
-      currentPlayer: 1,
-      board: [],
-      gameOver: false,
-      message: ''
-    };
-    
-    // Bind play function to App component
-    this.play = this.play.bind(this);
-  }
-  
-  // Starts new game
-  initBoard() {
-    // Create a blank 6x7 matrix
-    let board = [];
-    for (let r = 0; r < 6; r++) {
-      let row = [];
-      for (let c = 0; c < 7; c++) { row.push(null) }
-        board.push(row);
-    }
-    
-    this.setState({
-      board,
-
-    });
-  }
-  
-  togglePlayer() {
-    return (this.state.currentPlayer === this.state.player1) ? this.state.player2 : this.state.player1;
-  }
-  
-  play(c) {
-    if (!this.state.gameOver) {
-      // Place piece on board
-      let board = this.state.board;
-      for (let r = 5; r >= 0; r--) {
-        if (!board[r][c]) {
-          board[r][c] = this.state.currentPlayer;
-          break;
-        }
-      }
-
-      // Check status of board
-      let result = this.checkAll(board);
-      if (result === this.state.player1) {
-        this.setState({ board, gameOver: true, message: 'Player 1 (red) wins!' });
-      } else if (result === this.state.player2) {
-        this.setState({ board, gameOver: true, message: 'Player 2 (yellow) wins!' });
-      } else if (result === 'draw') {
-        this.setState({ board, gameOver: true, message: 'Draw game.' });
-      } else {
-        this.setState({ board, currentPlayer: this.togglePlayer() });
-      }
-    } else {
-      this.setState({ message: 'Game over. Please start a new game.' });
-    }
-  }
-  
-  checkVertical(board) {
-    // Check only if row is 3 or greater
-    for (let row = 3; row < 6; row++) {
-      for (let col = 0; col < 7; col++) {
-        if (board[row][col]) {
-          if (board[row][col] === board[row - 1][col] &&
-              board[row][col] === board[row - 2][col] &&
-              board[row][col] === board[row - 3][col]) {
-            return board[row][col];    
-          }
-        }
-      }
-    }
-  }
-  
-  checkHorizontal(board) {
-    // Check only if column is 3 or less
-    for (let row = 0; row < 6; row++) {
-      for (let col = 0; col < 4; col++) {
-        if (board[row][col]) {
-          if (board[row][col] === board[row][col + 1] && 
-              board[row][col] === board[row][col + 2] &&
-              board[row][col] === board[row][col + 3]) {
-            return board[row][col];
-          }
-        }
-      }
-    }
-  }
-  
-  checkDiagonalRight(board) {
-    // Check only if row is 3 or greater AND column is 3 or less
-    for (let row = 3; row < 6; row++) {
-      for (let c = 0; c < 4; c++) {
-        if (board[r][c]) {
-          if (board[row][col] === board[row - 1][col + 1] &&
-              board[row][col] === board[row - 2][col + 2] &&
-              board[row][col] === board[row - 3][col + 3]) {
-            return board[row][col];
-          }
-        }
-      }
-    }
-  }
-  
-  checkDiagonalLeft(board) {
-    // Check only if row is 3 or greater AND column is 3 or greater
-    for (let row = 3; row < 6; row++) {
-      for (let col = 3; col < 7; col++) {
-        if (board[row][c]) {
-          if (board[row][col] === board[row - 1][col - 1] &&
-              board[row][col] === board[row - 2][col - 2] &&
-              board[row][col] === board[row - 3][col - 3]) {
-            return board[row][col];
-          }
-        }
-      }
-    }
-  }
-  
-  checkDraw(board) {
-    for (let row = 0; row < 6; row++) {
-      for (let col = 0; col < 7; col++) {
-        if (board[row][col] === null) {
-          return null;
-        }
-      }
-    }
-    return 'draw';    
-  }
-  
-  checkAll(board) {
-    return this.checkVertical(board) || this.checkDiagonalRight(board) || this.checkDiagonalLeft(board) || this.checkHorizontal(board) || this.checkDraw(board);
-  }
-  
-  componentWillMount() {
-    this.initBoard();
-  }
-  
+class App extends Component {
   render() {
     return (
-      <div>
-        <div className="button" onClick={() => {this.initBoard()}}>New Game</div>
-        
-        <table>
-          <thead>
-          </thead>
-          <tbody>
-            {this.state.board.map((row, i) => (<Row key={i} row={row} play={this.play} />))}
-          </tbody>
-        </table>
-        
-        <p className="message">{this.state.message}</p>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route exact={true} path='/' render={() => (
+            <div className="App">
+              <SignIn />
+            </div>
+          )}/>
+          <Route exact={true} path='/game' render={() => (
+            <div className="App">
+              <Game />
+            </div>
+          )}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
-
-// Row component
-const Row = ({ row, play }) => {
-  return (
-    <tr>
-      {row.map((cell, i) => <Cell key={i} value={cell} columnIndex={i} play={play} />)}
-    </tr>
-  );
-};
-
-const Cell = ({ value, columnIndex, play }) => {
-  let color = 'noPlayer';
-  if (value === 1) {
-    color = 'player1color';
-  } else if (value === 2) {
-    color = 'player2color';
-  }
-    
-  return (
-    <td>
-      <div className="cell" onClick={() => {play(columnIndex)}}>
-        <div className={color}></div>
-      </div>
-    </td>
-  );
-};
-
-
 export default App;
---
+++ tutorial/src/SignIn.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Redirect} from 'react-router-dom';
+
+export default class SignUp extends Component { 
+constructor(props) {
+    super(props);
+    this.state = {valuePlayer1: '',
+                  valuePlayer2: '',
+                  toGame: false,
+                };
+
+    this.handleChangePlayer1 = this.handleChangePlayer1.bind(this);
+    this.handleChangePlayer2 = this.handleChangePlayer2.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    const player1 = this.props.player1;
+  }
+
+  handleChangePlayer1(event) {
+    this.setState({valuePlayer1: event.target.value});
+  }
+  handleChangePlayer2(event) {
+    this.setState({valuePlayer2: event.target.value});
+  }
+
+  handleSubmit(event) {
+    //this.props.history.push('/game');
+    this.setState({toGame: true});
+
+/*
+    if(this.state.valuePlayer2 != '' && this.state.valuePlayer1 != ''){
+      this.props.history.push('/game');
+    }
+    else{
+      alert('please enter names for both players');
+    }*/
+  }
+
+  render() {
+    if(this.state.toGame === true){
+      return <Redirect to ='/game' />
+    }
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Player 1 Name:
+          <input type="text" value={this.state.valuePlayer1} onChange={this.handleChangePlayer1} />
+        </label>
+        <label>
+          Player 2 Name:
+          <input type="text" value={this.state.valuePlayer2} onChange={this.handleChangePlayer2} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
